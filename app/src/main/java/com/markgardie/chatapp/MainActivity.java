10e89df9e4d91d9fb9c841d3261e2e2abcd51a7b
markgardie <markgardie@users.noreply.github.com>
2021-04-16 20:11:06 +0300

    added functions to MainActivity

--- app/src/main/java/com/markgardie/chatapp/MainActivity.java
+++ app/src/main/java/com/markgardie/chatapp/MainActivity.java
@@ -3,14 +3,26 @@ package com.markgardie.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SIGN_IN_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RelativeLayout activity_main = findViewById(R.id.activity_main);
 
+        if(FirebaseAuth.getInstance().getCurrentUser() == null)
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE);
+        else
+            Snackbar.make(activity_main, "Вы авторизованы", Snackbar.LENGTH_LONG).show();
     }
 }
