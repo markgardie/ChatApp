80ce39c8b567e2418f4237a9ac3679d762042859
markgardie <markgardie@users.noreply.github.com>
2021-04-19 20:01:59 +0300

    database functions

--- app/src/main/java/com/markgardie/chatapp/MainActivity.java
+++ app/src/main/java/com/markgardie/chatapp/MainActivity.java
@@ -1,28 +1,95 @@
 package com.markgardie.chatapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import android.text.format.DateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int SIGN_IN_CODE = 1;
+    private RelativeLayout activity_main;
+    private FirebaseListAdapter<Message> adapter;
+    private FloatingActionButton sendButton;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN_CODE) {
+            if(resultCode == RESULT_OK){
+                Snackbar.make(activity_main, "Вы авторизованы", Snackbar.LENGTH_LONG).show();
+                displayAllMessaged();
+            }
+            else {
+                Snackbar.make(activity_main, "Вы не авторизованы", Snackbar.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RelativeLayout activity_main = findViewById(R.id.activity_main);
+        activity_main = findViewById(R.id.activity_main);
+        sendButton = findViewById(R.id.btnSend);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text_field = findViewById(R.id.message_field);
+                if(text_field.getText().toString() == "")
+                    return;
+                FirebaseDatabase.getInstance().getReference().push().setValue(
+                        new Message(
+                                FirebaseAuth.getInstance().getCurrentUser().getEmail(),
+                                text_field.getText().toString()
+                                )
+                );
+
+                text_field.setText("");
+            }
+        });
 
         if(FirebaseAuth.getInstance().getCurrentUser() == null)
             startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE);
         else
             Snackbar.make(activity_main, "Вы авторизованы", Snackbar.LENGTH_LONG).show();
+            displayAllMessaged();
+
+    }
+
+    private void displayAllMessaged() {
+        ListView lisOfMessages = findViewById(R.id.list_messages);
+        adapter = new FirebaseListAdapter<Message>(this, Message.class, R.layout.list_item, FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View v, Message model, int position) {
+                TextView mess_user, mess_time, mess_text;
+                mess_user = v.findViewById(R.id.user);
+                mess_time = v.findViewById(R.id.time);
+                mess_text = v.findViewById(R.id.text);
+
+                mess_user.setText(model.getUserName());
+                mess_time.setText(DateFormat.format("dd-mm-yyyy HH:mm:ss", model.getMessageTime()));
+                mess_text.setText(model.getTextMessage());
+            }
+        };
+
+        lisOfMessages.setAdapter(adapter);
     }
 }
